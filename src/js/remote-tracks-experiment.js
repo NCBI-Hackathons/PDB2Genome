e6bef1498c5051f53e26c426d2e011e095a88663
Eric Weitz <eric.weitz@nih.gov>
2016-07-11 23:52:46 +0000

    Add uploaded track to existing track list (#2)

--- src/js/remote-tracks-experiment.js
+++ src/js/remote-tracks-experiment.js
@@ -1,13 +1,24 @@
 $ = jQuery;
 beds = {};
 $.getScript("http://104.196.135.118/2WNU_A.bed.js", function(bedData) {
-  console.log('ok');
+
+  // Use the NCBI User Uploaded Data (UUD) JS API to upload raw track data
   var f = new UUD.FileUploader({data: beds["2WNU_A"], track_name: "Test JS 2WNU_A"});
   f.upload();
+
   f.getPromise().done(function(tl) {
-    // id: GI of top-level sequence, here chr1 (NC_000001.11)
-    var url = 'id=568815597&tracks=[id:' + tl.GetTracks()[0].GetTMSId() + ']';
-    console.log("url: " + url);
-	SeqView.App.getApps()[0].reload(url);
+
+    var sv, viewParams, uploadedTrack;
+
+    // Get the Sequence Viewer (SV) instance
+    sv = SeqView.App.getApps()[0];
+
+    uploadedTrack = tl.GetTracks()[0];
+
+    sv.addUploadedTrackID(uploadedTrack);
+
+    viewParams = sv.m_AllViewParams;
+
+    sv.reload(viewParams);
   });
 });
