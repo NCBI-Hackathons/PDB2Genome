da06421de065f0e4498f088f1c931859b7032ff7
Eric Weitz <eric.weitz@nih.gov>
2016-07-12 13:15:15 +0000

    Update BED JS demo data, refine remote tracks script

--- src/js/remote-tracks-experiment.js
+++ src/js/remote-tracks-experiment.js
@@ -2,19 +2,19 @@
 //
 // To demo:
 // 1. Go to https://www.ncbi.nlm.nih.gov/genome/gdv/?context=genome&acc=GCF_000001405.33&q=C1QA
-// 2. Open your Chrome Developer Tools (Cmd-Alt-I)
+// 2. Open your web browser's Developer Tools (e.g. Cmd-Alt-I)
 // 3. Go to DevTools "Console" tab
 // 4. Paste the following into DevTools console:
 //    jQuery.getScript("https://ncbi-hackathons.github.io/PDB2Genome/remote-tracks-experiment.js");
 // 5. Press "Enter"
 // 6. See how the PDB structure track(s) have been added to bottom of Sequence Viewer track list
 
-pdbId = "2WNU_A"
+var stem = "chr2";
 
 // We serialize BED files into JavaScript strings to enable
 // experimental support for client-side requests of track
 // data from different origin servers.  This enables CORS.
-bedJsUrl = "https://ncbi-hackathons.github.io/PDB2Genome/bedjs/" + pdbId + ".bed.js";
+bedJsUrl = "https://ncbi-hackathons.github.io/PDB2Genome/bedjs/" + stem + ".bed.js"
 
 beds = {};
 jQuery.getScript(bedJsUrl, function(bedData) {
@@ -22,8 +22,8 @@ jQuery.getScript(bedJsUrl, function(bedData) {
   // Use the NCBI User Uploaded Data (UUD) JS API to upload raw track data
   var f = new UUD.FileUploader({
     assm_acc: "GCF_000001405.33",
-    data: beds[pdbId], 
-    track_name: "Residues for PDB ID " + pdbId + ", PDB2Genome, NCBI Orlando hackathon"
+    data: beds[stem], 
+    track_name: "Remote PDB track for " + stem + ", PDB2Genome, NCBI Orlando hackathon"
   });
   f.upload();
 
